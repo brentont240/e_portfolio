5afc59231cb396b7e93a1e46fe1ea8620f5c59b8
Brenton <brentontreb@gmail.com>
2025-05-01 21:26:05 -0600

    changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e_portfolio/'
+  base: '/e_portfolio/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html' // Explicit entry point
+    }
+  },
+  server: { // Fixes local dev
+    port: 3000,
+    open: true,
+  }
 })
