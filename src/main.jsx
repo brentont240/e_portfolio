fdcb549d4b90375524196bfe35140a40eb2f5ea8
Brenton <brentontreb@gmail.com>
2025-05-01 20:16:44 -0600

    Removed unnecessary styles and added CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './styles/theme';
-import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </StrictMode>
