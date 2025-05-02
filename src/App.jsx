e5c72e8d5f2372dd4462ce51061ab140f5fb4b16
Brenton <brentontreb@gmail.com>
2025-05-01 21:35:34 -0600

    Trying HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
   return(
-    <Router basename="/e_portfolio">
+    <Router>
       <Header />
         <main>
             <Routes>
