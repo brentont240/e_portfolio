48f78c6c71b8eb3cdb7f33dddd2aa5f11a2a9adc
Brenton Trebilcock <brentontreb@gmail.com>
2026-03-21 15:14:05 -0400

    Fixed resume link and link buttons on experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
     Box,
     Typography,
@@ -16,9 +16,25 @@ import SkillSection from "../components/SkillSection.jsx";
 import EducationCard from "../components/EducationCard.jsx";
 import WorkCard from "../components/WorkCard.jsx";
 import { CheckScreenSize } from "../utils/checkScreenSize.js";
+import { useLocation } from "react-router-dom";
 
 function Experience() {
-    // FIXME: get the resume link to scroll down to the resume section when clicked (top button!!!)
+    const location = useLocation();
+
+    const scrollToSection = (id, smooth) => {
+        const section = document.getElementById(id);
+        console.log(section);
+        if (section) {
+            section.scrollIntoView({ behavior: smooth ? "smooth" : "auto" });
+        }
+    };
+
+    useEffect(() => {
+        // jumps to resume section if the button is clicked in the navbar
+        if (location.hash === "#resume") {
+            scrollToSection("resume", false);
+        }
+    });
 
     return (
         <Box>
@@ -45,21 +61,43 @@ function Experience() {
                             size="large"
                             // FIXME: get this to work? or something similar?
                             // orientation={{ xs: "vertical", sm: "horizontal" }}
-                            orientation={CheckScreenSize("xs") ? "vertical" : "horizontal"}
+                            orientation={
+                                CheckScreenSize("xs")
+                                    ? "vertical"
+                                    : "horizontal"
+                            }
                         >
                             {/* TODO: IF I WANT LINKS TO THE DIFFERENT SECTIONS I CAN USE THIS (IDK IF IT'S NEEDED OR NOT?) */}
                             {/* FIXME: make it work better on mobile !!! */}
-                            <Button href="#skills">Skills</Button>
-                            <Button href="#education">Education</Button>
-                            <Button href="#workExperience">
+                            <Button
+                                onClick={() => scrollToSection("skills", true)}
+                            >
+                                Skills
+                            </Button>
+                            <Button
+                                onClick={() =>
+                                    scrollToSection("education", true)
+                                }
+                            >
+                                Education
+                            </Button>
+                            <Button
+                                onClick={() =>
+                                    scrollToSection("workExperience", true)
+                                }
+                            >
                                 Work Experience
                             </Button>
-                            <Button href="#resume">Resume</Button>
+                            <Button
+                                onClick={() => scrollToSection("resume", true)}
+                            >
+                                Resume
+                            </Button>
                         </ButtonGroup>
                     </Box>
                     {/* TODO: Add more skills and other things that aren't on resume!!! !!! !!! !!! */}
                     {/* FIXME: ADD VITE TESTING TO RESUME AND UPDATE AGRIPPON TEXT !!! !!! !!! !!! */}
-                    <Box sx={{ my: 2 }} id="skills">
+                    <Box sx={{ my: 2, scrollMarginTop: 70 }} id="skills">
                         <Divider className="sectionDividerPrimary">
                             <Typography variant="h2">Skills</Typography>
                         </Divider>
@@ -73,7 +111,7 @@ function Experience() {
                             skills={familiarSkills}
                         />
                     </Box>
-                    <Box sx={{ my: 2 }} id="education">
+                    <Box sx={{ my: 2, scrollMarginTop: 70 }} id="education">
                         <Divider
                             className="sectionDividerPrimary"
                             sx={{ mb: 2 }}
@@ -82,23 +120,29 @@ function Experience() {
                         </Divider>
                         <EducationCard education={education} />
                     </Box>
-                    <Box sx={{ my: 2 }} id="workExperience">
+                    <Box
+                        sx={{ my: 2, scrollMarginTop: 70 }}
+                        id="workExperience"
+                    >
                         <Divider
                             className="sectionDividerPrimary"
                             sx={{ mb: 2 }}
                         >
-                            {/* TODO: GET THIS TO WORK ON MOBILE (NOT JUST MAKE TEXT SMALLER!!!) */}
-                            {/* <Typography variant="h2" sx={{ whiteSpace: CheckScreenSize("xs") ? "normal" : "nowrap"}}> */}
-                            <Typography variant="h2" sx={{ whiteSpace: {
-                                "xs": "normal",
-                                "sm": "nowrap",
-                            }}}>
+                            <Typography
+                                variant="h2"
+                                sx={{
+                                    whiteSpace: {
+                                        xs: "normal",
+                                        sm: "nowrap",
+                                    },
+                                }}
+                            >
                                 Work Experience
                             </Typography>
                         </Divider>
                         <WorkCard work={work} />
                     </Box>
-                    <Box sx={{ my: 2 }} id="resume">
+                    <Box sx={{ my: 2, scrollMarginTop: 70 }} id="resume">
                         <Divider
                             className="sectionDividerPrimary"
                             sx={{ mb: 2 }}
