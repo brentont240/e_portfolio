ab939b0ff7241cdc17cb97d2f409b13e9d6cadfe
Brenton Trebilcock <brentontreb@gmail.com>
2026-03-25 19:27:02 -0400

    Changes to footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ROUTES } from "./routes";
-import Analytics from "./components/Analytics"
+import Analytics from "./components/Analytics";
+import { Box } from "@mui/material";
 // import ReactGA from "react-ga4";
 // import Home from "./pages/Home";
 
@@ -10,26 +11,34 @@ function App() {
     return (
         <Router basename="/e_portfolio">
             <Analytics />
-            <Navbar />
-            <main>
-                <Routes>
-                    {/* <Route path="/" element={<Home />} /> */}
-                    {/* ROUTES */}
-                    {ROUTES.map((route) => {
-                        // console.log(route)
-                        const Component = route.Component;
-                        const path = route.path;
-                        return (
-                            <Route
-                                path={path}
-                                key={path}
-                                element={<Component />}
-                            />
-                        );
-                    })}
-                </Routes>
-            </main>
-            <Footer />
+            <Box
+                sx={{
+                    minHeight: "100vh",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                <Navbar />
+                <Box component="main" sx={{ flex: 1 }}>
+                    <Routes>
+                        {/* <Route path="/" element={<Home />} /> */}
+                        {/* ROUTES */}
+                        {ROUTES.map((route) => {
+                            // console.log(route)
+                            const Component = route.Component;
+                            const path = route.path;
+                            return (
+                                <Route
+                                    path={path}
+                                    key={path}
+                                    element={<Component />}
+                                />
+                            );
+                        })}
+                    </Routes>
+                </Box>
+                <Footer />
+            </Box>
         </Router>
     );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,16 @@
 // import waveDivider from "../assets/layeredWaves.svg";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography, Container, Grid, Divider } from "@mui/material";
 
 // maybe get the thing to work idk????
 function Footer() {
     return (
+        // change to box component header?
         <footer>
             <Box
-            sx={{
-              // height: 100,
-              // backgroundColor: "action.disabledBackground"
-            }}
+                sx={{
+                    // height: 100,
+                    // backgroundColor: "action.disabledBackground"
+                }}
             >
                 {/* <Typography variant="h3" color="primary.main" align="center" mt={1}>Footer here?</Typography> */}
                 {/* <Box sx={{ width: "100%" }}>
@@ -22,6 +23,46 @@ function Footer() {
                     }}
                   />
             </Box> */}
+                <Paper
+                    variant="elevation"
+                    elevation="2"
+                    square={true}
+                    sx={{ 
+                      //  TODO: IS THIS NEEDED ANY MORE?
+                      // height: "100%" 
+                    }}
+                >
+                    <Container maxWidth="xl">
+                      <Grid container spacing={0} sx={{}}>
+                        <Grid size={{ xs: 12, md: 6 }}>
+                          <Typography
+                            variant="h3"
+                            color="primary.main"
+                            // align="center"
+                            mt={1}
+                        >
+                            Footer here?
+                        </Typography>
+                        </Grid>
+                      </Grid>
+                        {/* <Typography
+                            variant="h3"
+                            color="primary.main"
+                            align="center"
+                            mt={1}
+                        >
+                            Footer here?
+                        </Typography> */}
+                        <Divider
+                          variant="fullWidth"
+                          orientation="horizontal"
+                        />
+                        <Typography variant="body1" color="text.secondary" align="center" sx={{ }}>
+                          {/* see how to add spacing here??? */}
+                          © {new Date().getFullYear()} Brenton Trebilcock
+                        </Typography>
+                    </Container>
+                </Paper>
             </Box>
         </footer>
     );
